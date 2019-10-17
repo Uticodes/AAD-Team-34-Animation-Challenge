8a6b27b4f09598abff84d4a8bd1d645be9062f5e
Marwan <emef4z@gmail.com>
2019-10-17 18:30:44 +0400

    Add animation to splash screen

--- app/src/main/java/com/example/tekquotez/SplashScreen.java
+++ app/src/main/java/com/example/tekquotez/SplashScreen.java
@@ -3,15 +3,25 @@ package com.example.tekquotez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    ImageView splash;
+    Animation topdown;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        splash = (ImageView)findViewById(R.id.imageView);
+        topdown = AnimationUtils.loadAnimation(this,R.anim.topdown);
+        splash.setAnimation(topdown);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -21,7 +31,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        },1500);
+        },2000);
 
     }
 }
